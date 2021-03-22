0cd272d93f0a8a9c35a60b6ffc9526f1880647af
fabio98p <parisifabio98@gmail.com>
2021-03-22 15:38:25 +0100

    milestone2 js finito

--- Milestone/main.js
+++ Milestone/main.js
@@ -2,12 +2,25 @@ console.log("ciao ale");
 
 $(document).ready(() => {
 
-	icons.forEach(e => {
+	icons.forEach((e, index) => {
 		$("main").append(`<div>
 			<i class="${e.family} ${e.prefix}${e.name}"></i>
 			<div>${e.name}</div>
 		</div>`);
+		
+		$($("main i")[index]).css("color", () => {
+			if (e.type == "animal") {
+				console.log("blue");
+				return "blue"
+			} else if (e.type == "vegetable") {
+				console.log("orance");
+				return "orange"
+			} else if (e.type == "user") {
+				console.log("purple");
+				return "purple"
+			}
+		})
 	});
 
-	
+
 })
